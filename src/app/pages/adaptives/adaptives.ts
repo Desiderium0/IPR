794fe7ffb0ada22ad8f0b5a2d67f6477b82e4d45
Desik <vlad.kuptsov2000@mail.ru>
2025-08-03 20:14:32 +0700

    feat: added adaptive use flexbox

---
+++ src/app/pages/adaptives/adaptives.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adaptives',
+  imports: [],
+  templateUrl: './adaptives.html',
+  styleUrl: './adaptives.scss'
+})
+export class Adaptives {
+
+}
