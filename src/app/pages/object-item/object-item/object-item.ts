2d04eac8a3a8db641e3fcd1ff2e29b17f023452b
Desik <vlad.kuptsov2000@mail.ru>
2025-08-27 08:00:13 +0700

    refactor: Routing

--- src/app/pages/object-item/object-item/object-item.ts
+++ src/app/pages/object-item/object-item/object-item.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, RouterLink } from '@angular/router';
+import { MyObject, myObjects } from 'src/app/pages/object-list/object-list';
 
 @Component({
   selector: 'app-object-item',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './object-item.html',
-  styleUrl: './object-item.scss'
+  styleUrl: './object-item.scss',
 })
-export class ObjectItem {
+export class ObjectItem implements OnInit {
+  private _router = inject(ActivatedRoute);
+  protected objects?: MyObject;
 
+  public ngOnInit(): void {
+    this._router.params.subscribe((params: Params) => {
+      this.objects = myObjects[params['id'] - 1];
+    });
+  }
 }
